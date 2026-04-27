07b6f79ba36ec350215211fe615b09e763ac80ed
Milan <Milan.Poulias@uphf.fr>
2026-04-27 11:51:30 +0200

    Intégration de l'image

--- Joueur.py
+++ Joueur.py
@@ -5,19 +5,17 @@ from pygame.locals import *
 
 pygame.init() #initialisation
 
-pygame.key.set_repeat(100) # en ms
+pygame.key.set_repeat(10) # en ms | 10 fluide
 
 FENETRE = pygame.display.set_mode((1280, 720)) # Résolution HD, suffisant pour tester la classe joueur
-FENETRE.fill([255, 0, 0]) # On remplit la fenêtre de jeu de rouge
 
 class Joueur:
     def __init__(self, image:str)->None:
         """Création d'un nouveau Joueur et initialisation des variables"""
-        # self.image = pygame.image.load(image).convert()
-        # self.image = pygame.transform.smoothscale_by(self.image, 1)
-        # On dessine un rectangle pour l'instant mais on pourra charger des images plus tard
-        self.image = pygame.draw.rect(FENETRE, [0, 255, 0], pygame.Rect(0, 710, 10, 10)) # valeurs arbitraires
-        self.position = self.image
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.smoothscale_by(self.image, 1) # 1 = taille de base
+        self.position = self.image.get_rect()
+
     def __deplacement__(self)->None:
         """Gére les déplacements du Joueur :\n
         - q ou ← (flèche gauche) pour se déplacer à gauche\n
@@ -31,16 +29,15 @@ class Joueur:
             self.position.left += 1
         elif touche[K_SPACE] or touche[K_UP]:
             self.position.top -= 1
-        FENETRE.fill([255, 0, 0])
-        self.image = pygame.draw.rect(FENETRE, [0, 255, 0], self.position)
-        #FENETRE.blit(self.image, self.position) pour l'image
 
 
-Test = Joueur("test")
+Test = Joueur("./astro_neutre.png")
 
 #On teste la classe Joueur dans la boucle de jeu
 
 while True:
+    FENETRE.fill([255, 0, 0]) # On remplit la fenêtre de jeu de rouge
+    FENETRE.blit(Test.image, Test.position)
     pygame.display.flip() # on affiche notre perso
     for events in pygame.event.get():
         if events.type == QUIT: #si l'utilisateur clique sur la croix pour fermer la fenêtre, on ferme le jeu
